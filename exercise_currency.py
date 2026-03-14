c8dc158f5a26a5f9e6853c81c2d7d6ea2f8caad3
justoestevez <jeestevez@mail.austral.edu.ar>
2026-03-14 17:10:38 -0300

    Update exercise_currency.py

--- exercise_currency.py
+++ exercise_currency.py
@@ -11,3 +11,9 @@ def currency():
     tasa_dolar = 1500  # 1 dólar = 1500 pesos
     tasa_euro = 1600   # 1 euro = 1600 pesos
     tasa_real = 250    # 1 real = 250 pesos
+    dolares = pesos / tasa_dolar
+    euros = pesos / tasa_euro
+    reales_br = pesos / tasa_euro
+    print (dolares)
+    print (euros)
+    print (reales_br)
